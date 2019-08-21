dc51e18cbc33477e7ff30fe8bc68abeadb30cd97
nndrs <nandra.soack@gmail.com>
2019-08-22 00:02:31 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/submission1nandra/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submission1nandra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
