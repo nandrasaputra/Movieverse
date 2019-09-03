531656ce0c7cce7f02d3e1f2da3415a87afb7084
Nandra Saputra <nandra.soack@gmail.com>
2019-09-03 21:37:03 +0700

    V 3.0.0
    -Accepted Submission & Removing Unused Comments

--- app/src/main/java/com/nandra/moviecatalogue/ui/MainActivity.kt
+++ app/src/main/java/com/nandra/moviecatalogue/ui/MainActivity.kt
@@ -10,18 +10,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        return true
-    }*/
-
-    /*override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == R.id.main_menu_action_settings) {
-            findNavController(R.id.main_fragment_container).navigate(R.id.action_mainFragment_to_settingsPreferenceFragment)
-        }
-        return super.onOptionsItemSelected(item)
-    }*/
 }
